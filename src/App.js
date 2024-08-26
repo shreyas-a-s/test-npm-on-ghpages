a29cfb40287649db8c452c179f0cd5f135559fb6
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-08-26 20:16:21 +0530

    feat: Initial commmit
    - Added basic react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/NavBar";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
